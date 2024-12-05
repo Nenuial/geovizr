d613e7a9f06031fffc27059f904e4cd7eff16a69
Pascal Burkhard <pascal.burkhard@gmail.com>
2024-12-05 07:36:44 +0100

    feat: function to knit child documents

--- R/knit_children.R
+++ R/knit_children.R
@@ -1,20 +1,46 @@
 #' Knit child for MF oral exams
 #'
+#' Knits the child using `cat()` to
+#' print the output.
+#'
+#' This function should be used in a Quarto document inside
+#' a code chunk with the `asis` option.
+#'
 #' @param ... Data that is passed to the fragment
 #'   Must have *ID*, *Name*, *Subject* and *Question*
 #'
-#' @return Knitted document
+#' @return NULL
 #' @export
-gvz_knit_child_matu_oraux <- function(...) {
-  data <- tibble::tibble(...)
+gvz_knit_child <- function (input, ...) {
+  out <- rlang::list2(...)
+  data <- tibble::tibble(!!!out)
 
   child <- knitr::knit_child(
-    input = system.file("rmarkdown/templates/Matu_oraux/resources/matu_oraux_fragment.Rmd",
-      package = "geovizr"
-    ),
+    input = input,
     quiet = TRUE,
     envir = environment()
   )
 
   cat(child)
+}
+
+#' Knit child for MF oral exams
+#'
+#' Knits the child for the Matu oral exams
+#' using `cat()` to print the output.
+#'
+#' @param ... Data that is passed to the fragment
+#'   Must have *ID*, *Name*, *Subject* and *Question*
+#'
+#' @return NULL
+#' @export
+gvz_knit_child_matu_oraux <- function(...) {
+  out <- rlang::list2(...)
+
+  gvz_knit_child(
+    input = system.file("rmarkdown/templates/Matu_oraux/resources/matu_oraux_fragment.Rmd",
+      package = "geovizr"
+    ),
+    !!!out
+  )
 }
